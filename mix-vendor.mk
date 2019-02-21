e8363a6f7455c13129fdf6227f6faa5a8ce1cbb4
jmpf.bmx <jmpf.bmx@gmail.com>
2019-02-21 17:21:36 +0100

    first commit

---
+++ mix-vendor.mk
@@ -0,0 +1,11 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/doogee/mix/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/doogee/mix/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/doogee/mix/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/doogee/mix/usr,system/usr)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/doogee/mix/vendor,system/vendor)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/doogee/mix/lib64,system/lib64)
